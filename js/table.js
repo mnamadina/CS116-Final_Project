d5f8d2076a1caf0c34f275c3a5d8bc9120925a45
mnamadina <madina@brandeis.edu>
2023-11-21 21:50:48 -0500

    sony's table

---
+++ js/table.js
@@ -0,0 +1,75 @@
+// Sony's code
+function Table() {
+    function chart(selector, data) {
+        let ourBrush = null,
+        selectableElements = d3.select(null),
+        dispatcher;
+        
+        let table = d3.select(selector)
+            .append("table")
+            .classed("my-table", true);
+
+        let tableHeaders = ["State", "Total Healthcare Expenditure", "Spending on Medicaid", "Health Insurance Coverage Percentage", "Maternity Deaths"];
+
+        let tr = table.append('thead').append('tr');
+        tr.selectAll('th').data(tableHeaders).enter().append('th').text((d) => d);
+
+        let rows = table.append('tbody').selectAll('tr')
+            .data(data)
+            .enter()
+            .append('tr');
+
+        rows.selectAll('td')
+            .data(d => [d.column1, d.column2, d.column3, d.column4, d.column5])
+            .enter()
+            .append('td')
+            .text(d => d);
+
+        rows.on("mouseover", (d, i, elements) => {
+            if (ourBrush !== null) {
+              d3.select(elements[i]).classed("highlighted", true);
+              dispatcher.call("highlight", this, d);
+            }
+          }).on("mouseout", (d, i, elements) => {
+            if (ourBrush !== null) {
+              d3.select(elements[i]).classed("highlighted", false);
+              dispatcher.call("unhighlight", this);
+            }
+          });
+      
+          rows.on("mousedown", (d, i, elements) => {
+            ourBrush = d3.select(elements[i]);
+            ourBrush.classed("mousedown", true);
+            selectableElements = d3.selectAll(".highlighted");
+            dispatcher.call("selected", this, selectableElements.data());
+          });
+      
+          d3.select("body").on("mouseup", () => {
+            if (ourBrush !== null) {
+              ourBrush.classed("mousedown", false);
+              selectableElements = d3.select(null);
+              ourBrush = null;
+            }
+          });
+
+        return chart;
+    }
+    chart.selectionDispatcher = function (_) {
+        if (!arguments.length) return dispatcher;
+        dispatcher = _;
+        return chart;
+      };
+    
+      // Used to select relevant information from our other visualizations.
+      // Will be modified for linking
+      chart.updateSelection = function (selectedData) {
+        if (!arguments.length) return;
+    
+        // Select an element if its datum was selected
+        d3.selectAll('tr').classed("selected", d => {
+          return selectedData.includes(d)
+        });
+      };
+
+    return chart;
+}
