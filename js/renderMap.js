d318cf5847c6f86f62de46eb4dfed760dc210332
persiad <sky980218@gmail.com>
2023-11-29 16:22:48 -0500

    added map

---
+++ js/renderMap.js
@@ -0,0 +1,25 @@
+(async () => {
+    // Step 2. Load the US map data.
+    const us = await d3.json('https://d3js.org/us-10m.v2.json');
+    const data = topojson.feature(us, us.objects.states).features;
+
+    // Step 3. Draw the SVG.
+    // First let's create an empty SVG with 960px width and 600px height.
+    const width = 960;
+    const height = 600;
+    const svg = d3.select('body')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    // Create an instance of geoPath.
+    const path = d3.geoPath();
+
+    // Use the path to plot the US map based on the geometry data.
+    svg.append('g')
+      .selectAll('path')
+      .data(data)
+      .enter()
+      .append('path')
+      .attr('d', path);
+  })();
