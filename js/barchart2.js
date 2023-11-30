586784ee644f50f55d0575e3b101a1a9aeee434e
Brooke Schwartz <brookedschwartz@gmail.com>
2023-11-30 16:28:24 -0500

    new barchart lol

--- js/barchart2.js
+++ js/barchart2.js
@@ -1,10 +1,8 @@
-/* global D3 */
-
-console.log("barchart file");
-
+// Immediately Invoked Function Expression to limit access to our 
+// variables and prevent 
+// console.log("visualization.js is up!");
 (() => {
 
-  // Will be edited to follow the path later
   var barchart_data =
   [
     {"State": "United States", "Insured": 91.4},
@@ -60,89 +58,57 @@ console.log("barchart file");
     {"State": "Wisconsin", "Insured": 94.6},
     {"State": "Wyoming", "Insured": 87.8}
   ]
+  var margin = { top: 30, right: 30, bottom: 70, left: 60 };
+  var width = 800 - margin.left - margin.right;
+  var height = 500 - margin.top - margin.bottom;
 
 
-/* global D3 */
-let chart = barchart();
-  chart("#chart-container", barchart_data);
+  var svg = d3.select('#barchart') // Assuming 'barchart' is the container ID
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-function barchart() {
-  console.log("setting the measurements for the barchart");
+  var xScale = d3.scaleBand().range([0, width]).padding(0.1);
+  var yScale = d3.scaleLinear().range([height, 0]);
 
-  // Based on Mike Bostock's margin convention
-  // https://bl.ocks.org/mbostock/3019563
-  let margin = {
-    top: 60,
-    left: 50,
-    right: 30,
-    bottom: 35
-  },
+  // Domain for xScale (states)
+  xScale.domain(barchart_data.map(d => d.State));
 
-    width = 500 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom,
-    xValue = d => d[0],
-    yValue = d => d[1],
-    xLabelText = "",
-    yLabelText = "",
-    yLabelOffsetPx = 0,
-    xScale = d3.scalePoint(),
-    yScale = d3.scaleLinear(),
-    dispatcher;
+  // Domain for yScale (values)
+  yScale.domain([0, d3.max(barchart_data, d => d.Insured)]);
 
+  // Append bars to the SVG
+  svg.selectAll('.bar')
+    .data(barchart_data)
+    .enter().append('rect')
+    .attr('class', 'bar')
+    .attr('x', d => xScale(d.State))
+    .attr('width', xScale.bandwidth())
+    .attr('y', d => yScale(d.Insured))
+    .attr('height', d => height - yScale(d.Insured))
+    .attr('fill', 'steelblue'); // Bar color, you can change this as needed
 
-  // Create the chart by adding an svg to the div with the id 
-  // specified by the selector using the given data
-  function chart(selector, data) {
-    console.log("calling the chart in the barchart");
-    let svg = d3.select(selector)
-      .append("svg")
-      .attr("preserveAspectRatio", "xMidYMid meet")
-      .attr("viewBox", [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
-      .classed("svg-content", true);
+  // Append x-axis
+  svg.append('g')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(d3.axisBottom(xScale))
+    .selectAll('text')
+    .attr('dy', '.35em')
+    .attr('transform', 'rotate(45)')
+    .style('text-anchor', 'start');
 
-    svg = svg.append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  // Append y-axis
+  svg.append('g')
+    .call(d3.axisLeft(yScale));
 
-    // Define scales
-    // Update xScale and yScale domains based on data
-    xScale
-      .domain(barchart_data.map(d => d.State))
-      .range([0, width]);
-
-    yScale
-      .domain([0, d3.max(barchart_data, d => d.Insured)])
-      .nice()
-      .range([height, 0]);
-
-    // X axis
-    let xAxis = svg.append("g")
-      .attr("transform", "translate(0," + (height) + ")")
-      .call(d3.axisBottom(xScale));
-
-    // Put X axis tick labels at an angle
-    xAxis.selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", ".15em")
-      .attr("transform", "rotate(-65)");
-
-    // X axis label
-    xAxis.append("text")
-      .attr("class", "axisLabel")
-      .attr("transform", "translate(" + (width - 50) + ",-10)")
-      .text(xLabelText);
-
-    // Y axis and label
-    let yAxis = svg.append("g")
-      .call(d3.axisLeft(yScale))
-      .append("text")
-      .attr("class", "axisLabel")
-      .attr("transform", "translate(" + yLabelOffsetPx + ", -12)")
-      .text(yLabelText);
-
-    return chart;
-  }
-
-  return chart;
-}
-})
+  // Append y-axis label
+  svg.append('text')
+    .attr('class', 'axis-label')
+    .attr('transform', 'rotate(-90)')
+    .attr('x', -height / 2)
+    .attr('y', -margin.left + 20) // Position the label to the left of the y-axis
+    .style('text-anchor', 'middle')
+    .text('Insured Percentage'); // Change this to your y-axis label text
+})();
