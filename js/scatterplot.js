87a4949d9557a93f51312933e814d9d3731eb5d9
Brooke Schwartz <brookedschwartz@gmail.com>
2023-11-30 14:27:34 -0500

    bork

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -1,212 +1,296 @@
-// Initialize a scatterplot. Modeled after Mike Bostock's
-// Reusable Chart framework https://bost.ocks.org/mike/chart/
-function scatterplot() {
+// Immediately Invoked Function Expression to limit access to our 
+// variables and prevent 
+// console.log("visualization.js is up!");
+(() => {
 
-  // Based on Mike Bostock's margin convention
-  // https://bl.ocks.org/mbostock/3019563
-  let margin = {
-      top: 60,
-      left: 50,
-      right: 30,
-      bottom: 20
-    },
-    width = 500 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom,
-    xValue = d => d[0],
-    yValue = d => d[1],
-    xLabelText = "",
-    yLabelText = "",
-    yLabelOffsetPx = 0,
-    xScale = d3.scaleLinear(),
-    yScale = d3.scaleLinear(),
-    ourBrush = null,
-    selectableElements = d3.select(null),
-    dispatcher;
+  // Will be edited to follow the path later
+  var scatterplot_data =
+    [
+      {
+        "State": "California",
+        "Ranking": 1,
+        "Mortality deaths/100,000 births": 9.5,
+        "% living below poverty line (2021)": 12.3,
+        "% of non-white residents": 26.23,
+        "Instances of hate crimes (per 100,000)": 2.76
+      },
+      {
+        "State": "Wisconsin",
+        "Ranking": 2,
+        "Mortality deaths/100,000 births": 10.8,
+        "% living below poverty line (2021)": 10.8,
+        "% of non-white residents": 26.47,
+        "Instances of hate crimes (per 100,000)": 1.34
+      },
+      {
+        "State": "Rhode Island",
+        "Ranking": 3,
+        "Mortality deaths/100,000 births": 11.2,
+        "% living below poverty line (2021)": 12.1,
+        "% of non-white residents": 20.63,
+        "Instances of hate crimes (per 100,000)": 1.9
+      },
+      {
+        "State": "Minnesota",
+        "Ranking": 4,
+        "Mortality deaths/100,000 births": 12.4,
+        "% living below poverty line (2021)": 9.3,
+        "% of non-white residents": 23.44,
+        "Instances of hate crimes (per 100,000)": 3.71
+      },
+      {
+        "State": "Massachusetts",
+        "Ranking": 5,
+        "Mortality deaths/100,000 births": 14.2,
+        "% living below poverty line (2021)": 10.4,
+        "% of non-white residents": 6.32,
+        "Instances of hate crimes (per 100,000)": 5.77
+      },
+      {
+        "State": "Utah",
+        "Ranking": 6,
+        "Mortality deaths/100,000 births": 14.9,
+        "% living below poverty line (2021)": 8.7,
+        "% of non-white residents": 23.27,
+        "Instances of hate crimes (per 100,000)": 2.45
+      },
+      {
+        "State": "Pennsylvania",
+        "Ranking": 7,
+        "Mortality deaths/100,000 births": 15.6,
+        "% living below poverty line (2021)": 12,
+        "% of non-white residents": 28.85,
+        "Instances of hate crimes (per 100,000)": 0.42
+      },
+      {
+        "State": "Colorado",
+        "Ranking": 8,
+        "Mortality deaths/100,000 births": 15.9,
+        "% living below poverty line (2021)": 9.7,
+        "% of non-white residents": 24.63,
+        "Instances of hate crimes (per 100,000)": 3.73
+      },
+      {
+        "State": "Connecticut",
+        "Ranking": 9,
+        "Mortality deaths/100,000 births": 16.7,
+        "% living below poverty line (2021)": 10.1,
+        "% of non-white residents": 43.95,
+        "Instances of hate crimes (per 100,000)": 3.91
+      },
+      {
+        "State": "New Hampshire",
+        "Ranking": 10,
+        "Mortality deaths/100,000 births": 16.7,
+        "% living below poverty line (2021)": 7.4,
+        "% of non-white residents": 14.69,
+        "Instances of hate crimes (per 100,000)": 1.87
+      },
+      {
+        "State": "Oregon",
+        "Ranking": 11,
+        "Mortality deaths/100,000 births": 16.8,
+        "% living below poverty line (2021)": 12.2,
+        "% of non-white residents": 19.53,
+        "Instances of hate crimes (per 100,000)": 5.25
+      },
+      {
+        "State": "Hawaii",
+        "Ranking": 12,
+        "Mortality deaths/100,000 births": 16.9,
+        "% living below poverty line (2021)": 10.9,
+        "% of non-white residents": 28.36,
+        "Instances of hate crimes (per 100,000)": null
+      },
+      {
+        "State": "Illinois",
+        "Ranking": 13,
+        "Mortality deaths/100,000 births": 16.9,
+        "% living below poverty line (2021)": 12.1,
+        "% of non-white residents": 75.85,
+        "Instances of hate crimes (per 100,000)": 0.69
+      },
+      {
+        "State": "Idaho",
+        "Ranking": 14,
+        "Mortality deaths/100,000 births": 18.2,
+        "% living below poverty line (2021)": 10.8,
+        "% of non-white residents": 42.75,
+        "Instances of hate crimes (per 100,000)": 2.08
+      },
+      {
+        "State": "Michigan",
+        "Ranking": 15,
+        "Mortality deaths/100,000 births": 18.4,
+        "% living below poverty line (2021)": 13,
+        "% of non-white residents": 45.76,
+        "Instances of hate crimes (per 100,000)": 3.62
+      },
+      {
+        "State": "Iowa",
+        "Ranking": 16,
+        "Mortality deaths/100,000 births": 18.7,
+        "% living below poverty line (2021)": 11,
+        "% of non-white residents": 30.21,
+        "Instances of hate crimes (per 100,000)": 0.52
+      },
+      {
+        "State": "Delaware",
+        "Ranking": 17,
+        "Mortality deaths/100,000 births": 18.9,
+        "% living below poverty line (2021)": 11.5,
+        "% of non-white residents": 18.48,
+        "Instances of hate crimes (per 100,000)": 1.65
+      },
+      {
+        "State": "New York",
+        "Ranking": 18,
+        "Mortality deaths/100,000 births": 19.8,
+        "% living below poverty line (2021)": 14,
+        "% of non-white residents": 34.5,
+        "Instances of hate crimes (per 100,000)": 2.82
+      },
+      {
+        "State": "Washington",
+        "Ranking": 19,
+        "Mortality deaths/100,000 births": 20.4,
+        "% living below poverty line (2021)": 9.9,
+        "% of non-white residents": 6.4,
+        "Instances of hate crimes (per 100,000)": 3.12
+      }
+  ];
+  var margin = { top: 20, right: 20, bottom: 50, left: 70 };
+  var width = 800 - margin.left - margin.right;
+  var height = 500 - margin.top - margin.bottom;
 
-  // Create the chart by adding an svg to the div with the id 
-  // specified by the selector using the given data
-  function chart(selector, data) {
-    let svg = d3.select(selector)
-      .append("svg")
-        .attr("preserveAspectRatio", "xMidYMid meet")
-        .attr("viewBox", [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
-        .classed("svg-content", true);
+  var svg = d3.select('#scatterplot')
+    .append('svg')
+    .attr('id', 'vis-svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    svg = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var xScale = d3.scaleLinear().range([0, width]);
+  var yScale = d3.scaleLinear().range([height, 0]);
 
-    //Define scales
-    xScale
-      .domain([
-        d3.min(data, d => xValue(d)),
-        d3.max(data, d => xValue(d))
-      ])
-      .rangeRound([0, width]);
+  xScale.domain([d3.min(scatterplot_data, d => d["% of non-white residents"]), d3.max(scatterplot_data, d => d["% of non-white residents"])]);
+  yScale.domain([d3.min(scatterplot_data, d => d["Mortality deaths/100,000 births"]), d3.max(scatterplot_data, d => d["Mortality deaths/100,000 births"])]);
 
-    yScale
-      .domain([
-        d3.min(data, d => yValue(d)),
-        d3.max(data, d => yValue(d))
-      ])
-      .rangeRound([height, 0]);
+  svg.append('g')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(d3.axisBottom(xScale))
+    .append('text')
+    .attr('class', 'axis-label')
+    .attr('x', width / 2)
+    .attr('y', 40)
+    .style('text-anchor', 'middle')
+    .text('% of non-white residents');
 
-    let xAxis = svg.append("g")
-        .attr("transform", "translate(0," + (height) + ")")
-        .call(d3.axisBottom(xScale));
-        
-    // X axis label
-    xAxis.append("text")        
-        .attr("class", "axisLabel")
-        .attr("transform", "translate(" + (width - 50) + ",-10)")
-        .text(xLabelText);
-      
-    let yAxis = svg.append("g")
-        .call(d3.axisLeft(yScale))
-      .append("text")
-        .attr("class", "axisLabel")
-        .attr("transform", "translate(" + yLabelOffsetPx + ", -12)")
-        .text(yLabelText);
+  svg.append('g')
+    .call(d3.axisLeft(yScale))
+    .append('text')
+    .attr('class', 'axis-label')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', -60)
+    .attr('x', -height / 2)
+    .style('text-anchor', 'middle')
+    .text('Mortality deaths/100,000 births');
 
-    // Add the points
-    let points = svg.append("g")
-      .selectAll(".scatterPoint")
-        .data(data);
+    var circles = svg.selectAll('circle')
+    .data(scatterplot_data)
+    .enter().append('circle')
+    .attr('cx', d => xScale(d["% of non-white residents"]))
+    .attr('cy', d => yScale(d["Mortality deaths/100,000 births"]))
+    .attr('r', 8)
+    .attr('class', 'scatter-circle');
 
-    points.exit().remove();
+  circles.on('mouseover', function (event, d) {
+    d3.select(this)
+      .attr('r', 12) // Increase circle radius on hover for highlighting effect
+      .append('text')
+      .attr('class', 'dot-label')
+      .attr('x', xScale(d["% of non-white residents"]) + 10)
+      .attr('y', yScale(d["Mortality deaths/100,000 births"]) - 10)
+      .text(d.State);
+  })
+  .on('mouseout', function () {
+    d3.select(this)
+      .attr('r', 8); // Reset circle radius on mouseout
+    d3.selectAll('.dot-label').remove(); // Remove all dot labels on mouseout
+  });
 
-    points = points.enter()
-      .append("circle")
-        .attr("class", "point scatterPoint")
-      .merge(points)
-        .attr("cx", X)
-        .attr("cy", Y)
-        .attr("r", 5);
-    
-    selectableElements = points;
-    
-    svg.call(brush);
+      // Adding x-axis label
+  svg.append('text')
+  .attr('class', 'axis-label')
+  .attr('x', width / 2)
+  .attr('y', height + margin.top + 25) // Position the label below the x-axis
+  .style('text-anchor', 'middle')
+  .text('% of non-white residents');
 
-    // Highlight points when brushed
-    function brush(g) {
-      const brush = d3.brush() // Create a 2D interactive brush
-        .on("start brush", highlight) // When the brush starts/continues do...
-        .on("end", brushEnd) // When the brush ends do...
-        .extent([
-          [-margin.left, -margin.bottom],
-          [width + margin.right, height + margin.top]
-        ]);
-        
-      ourBrush = brush;
+// Adding y-axis label
+svg.append('text')
+  .attr('class', 'axis-label')
+  .attr('transform', 'rotate(-90)')
+  .attr('x', -height / 2)
+  .attr('y', -margin.left + 20) // Position the label to the left of the y-axis
+  .style('text-anchor', 'middle')
+  .text('Mortality deaths/100,000 births');
 
-      g.call(brush); // Adds the brush to this element
+  svg.selectAll('.text-label')
+    .data(scatterplot_data)
+    .enter().append('text')
+    .attr('class', 'text-label')
+    .attr('x', d => xScale(d["% of non-white residents"]) + 10) 
+    .attr('y', d => yScale(d["Mortality deaths/100,000 births"]) - 10) // Adjust the position to better fit the labels
+    .text(d => d.State);
 
-      // Highlight the selected circles
+  function brush() {
+    const brush = d3.brush()
+      .on("start brush", highlight) 
+      .on("end", brushEnd) 
+      .extent([
+        [-margin.left, -margin.bottom],
+        [width + margin.right, height + margin.top]
+      ]);
+
+    svg.append('g')
+      .call(brush);
+
       function highlight() {
-        if (d3.event.selection === null) return;
         const [
           [x0, y0],
           [x1, y1]
         ] = d3.event.selection;
-
-        // If within the bounds of the brush, select it
-        points.classed("selected", d =>
-          x0 <= X(d) && X(d) <= x1 && y0 <= Y(d) && Y(d) <= y1
+  
+        circles.classed('selected', d =>
+          x0 <= xScale(d["% of non-white residents"]) && xScale(d["% of non-white residents"]) <= x1 &&
+          y0 <= yScale(d["Mortality deaths/100,000 births"]) && yScale(d["Mortality deaths/100,000 births"]) <= y1
         );
-
-        // Get the name of our dispatcher's event
-        let dispatchString = Object.getOwnPropertyNames(dispatcher._)[0];
-
-        // Let other charts know about our selection
-        dispatcher.call(dispatchString, this, svg.selectAll(".selected").data());
+  
+        circles.attr('fill', function () {
+          return d3.select(this).classed('selected') ? 'red' : ''; // Apply red fill to selected circles
+        });
       }
-      
-      function brushEnd(){
-        // We don't want infinite recursion
-        if(d3.event.sourceEvent.type!="end"){
-          d3.select(this).call(brush.move, null);
-        }         
+
+    function brushEnd() {
+      // We don't want infinite recursion
+      if (d3.event.sourceEvent.type != "end") {
+        d3.select(this).call(brush.move, null);
       }
     }
-
-    return chart;
   }
 
-  // The x-accessor from the datum
-  function X(d) {
-    return xScale(xValue(d));
-  }
+  brush(); 
 
-  // The y-accessor from the datum
-  function Y(d) {
-    return yScale(yValue(d));
-  }
+  // Adding mousedown event listener to document body
+  document.body.addEventListener('mousedown', function (event) {
+    const isClickedInsideCircle = event.target.closest('.scatter-circle');
+    const isClickedInsideSelected = event.target.closest('.selected');
 
-  chart.margin = function (_) {
-    if (!arguments.length) return margin;
-    margin = _;
-    return chart;
-  };
-
-  chart.width = function (_) {
-    if (!arguments.length) return width;
-    width = _;
-    return chart;
-  };
-
-  chart.height = function (_) {
-    if (!arguments.length) return height;
-    height = _;
-    return chart;
-  };
-
-  chart.x = function (_) {
-    if (!arguments.length) return xValue;
-    xValue = _;
-    return chart;
-  };
-
-  chart.y = function (_) {
-    if (!arguments.length) return yValue;
-    yValue = _;
-    return chart;
-  };
-
-  chart.xLabel = function (_) {
-    if (!arguments.length) return xLabelText;
-    xLabelText = _;
-    return chart;
-  };
-
-  chart.yLabel = function (_) {
-    if (!arguments.length) return yLabelText;
-    yLabelText = _;
-    return chart;
-  };
-
-  chart.yLabelOffset = function (_) {
-    if (!arguments.length) return yLabelOffsetPx;
-    yLabelOffsetPx = _;
-    return chart;
-  };
-
-  // Gets or sets the dispatcher we use for selection events
-  chart.selectionDispatcher = function (_) {
-    if (!arguments.length) return dispatcher;
-    dispatcher = _;
-    return chart;
-  };
-
-  // Given selected data from another visualization 
-  // select the relevant elements here (linking)
-  chart.updateSelection = function (selectedData) {
-    if (!arguments.length) return;
-
-    // Select an element if its datum was selected
-    selectableElements.classed("selected", d => {
-      return selectedData.includes(d)
-    });
-
-  };
-
-  return chart;
-}
+    if (!isClickedInsideCircle && !isClickedInsideSelected) {
+      // Remove 'selected' class and reset circle colors
+      circles.classed('selected', false).attr('fill', ''); // Remove 'selected' class and reset fill color
+    }
+  });
+})();
