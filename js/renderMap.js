41f49dedb9133be67b09db71d70a56c579d3cbeb
persiad <sky980218@gmail.com>
2023-11-30 23:00:17 -0500

    Updated renderMap.js. Map now supports full brushing functionality.

--- js/renderMap.js
+++ js/renderMap.js
@@ -1,56 +1,60 @@
 (async () => {
-  console.log("working");
-  
-    // Step 2. Load the US map data.
-    const us = await d3.json('https://d3js.org/us-10m.v2.json');
-    const data = topojson.feature(us, us.objects.states).features;
+  // Step 2. Load the US map data.
+  const us = await d3.json('https://d3js.org/us-10m.v2.json');
+  const data = topojson.feature(us, us.objects.states).features;
 
-    // Step 3. Draw the SVG.
-    // First let's create an empty SVG with 960px width and 600px height.
-    const width = 960;
-    const height = 600;
-    const svg = d3.select('body')
+  // Step 3. Draw the SVG.
+  // First let's create an empty SVG with 960px width and 600px height.
+  const width = 960;
+  const height = 600;
+  const svg = d3.select('body')
       .append('svg')
       .attr('width', width)
       .attr('height', height);
-    // const svg = d3.select('#map')
-    //       .attr('width', width)
-    //       .attr('height', height);
 
-    // Create an instance of geoPath.
-    const path = d3.geoPath();
+  // Create an instance of geoPath.
+  const path = d3.geoPath();
 
-    // Use the path to plot the US map based on the geometry data.
-    svg.append('g')
+  // Function to handle mouseover event
+  function handleMouseOver(d) {
+      d3.select(this)
+          .transition()
+          .duration(200)
+          .style('fill', 'bisque'); // Change the color on mouseover
+  }
+
+  // Function to handle mouseleave event
+  function handleMouseLeave(d) {
+      d3.select(this)
+          .transition()
+          .duration(200)
+          .style('fill', 'cornflowerblue'); // Change back to original color on mouseleave
+  }
+
+  // Function to handle click event
+  function handleMouseClick(d) {
+      // Toggle the highlighted state on click
+      const currentState = d3.select(this);
+      const isHighlighted = currentState.classed('highlighted');
+
+      currentState.classed('highlighted', !isHighlighted);
+
+      // Change the color based on whether the state is highlighted
+      currentState.transition()
+          .duration(200)
+          .style('fill', isHighlighted ? 'cornflowerblue' : 'lightpink');
+  }
+
+  // Use the path to plot the US map based on the geometry data.
+  svg.append('g')
       .selectAll('path')
       .data(data)
       .enter()
       .append('path')
-      .attr('d', path);
-  
-  //     function ready(error, topo) {
-
-  //       let mouseOver = function(d) {
-  //         d3.selectAll(".state")
-  //           .transition()
-  //           .duration(200)
-  //           .style("opacity", .5)
-  //         d3.select(this)
-  //           .transition()
-  //           .duration(200)
-  //           .style("opacity", 1)
-  //           .style("stroke", "black")
-  //       }
-      
-  //       let mouseLeave = function(d) {
-  //         d3.selectAll(".state")
-  //           .transition()
-  //           .duration(200)
-  //           .style("opacity", .8)
-  //         d3.select(this)
-  //           .transition()
-  //           .duration(200)
-  //           .style("stroke", "transparent")
-  //       }
-  // }
+      .attr('class', 'state')
+      .attr('d', path)
+      .style('fill', 'cornflowerblue')
+      .on('mouseover', handleMouseOver)
+      .on('mouseleave', handleMouseLeave)
+      .on('click', handleMouseClick);
 })();
