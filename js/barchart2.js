01db182b7c1d5e0460ce04f59e82aedce9ca9af0
persiad <sky980218@gmail.com>
2023-12-10 21:52:00 -0500

    updated scatterplot.js and barchart2.js to fetch data using d3.json

--- js/barchart2.js
+++ js/barchart2.js
@@ -2,61 +2,7 @@
 // variables and prevent 
 // console.log("visualization.js is up!");
 (() => {
-
-  var barchart_data = [
-    {"State": "Alabama", "Insured": 90.1},
-    {"State": "Alaska", "Insured": 88.6},
-    {"State": "Arizona", "Insured": 89.3},
-    {"State": "Arkansas", "Insured": 90.8},
-    {"State": "California", "Insured": 93},
-    {"State": "Colorado", "Insured": 92},
-    {"State": "Connecticut", "Insured": 94.8},
-    {"State": "Delaware", "Insured": 94.3},
-    {"State": "District of Columbia", "Insured": 96.3},
-    {"State": "Florida", "Insured": 87.9},
-    {"State": "Georgia", "Insured": 87.4},
-    {"State": "Hawaii", "Insured": 96.1},
-    {"State": "Idaho", "Insured": 91.2},
-    {"State": "Illinois", "Insured": 93},
-    {"State": "Indiana", "Insured": 92.5},
-    {"State": "Iowa", "Insured": 95.2},
-    {"State": "Kansas", "Insured": 90.8},
-    {"State": "Kentucky", "Insured": 94.3},
-    {"State": "Louisiana", "Insured": 92.4},
-    {"State": "Maine", "Insured": 94.3},
-    {"State": "Maryland", "Insured": 93.9},
-    {"State": "Massachusetts", "Insured": 97.5},
-    {"State": "Michigan", "Insured": 95},
-    {"State": "Minnesota", "Insured": 95.5},
-    {"State": "Mississippi", "Insured": 88.1},
-    {"State": "Missouri", "Insured": 90.6},
-    {"State": "Montana", "Insured": 91.8},
-    {"State": "Nebraska", "Insured": 92.9},
-    {"State": "Nevada", "Insured": 88.4},
-    {"State": "New Hampshire", "Insured": 94.9},
-    {"State": "New Jersey", "Insured": 92.8},
-    {"State": "New Mexico", "Insured": 90},
-    {"State": "New York", "Insured": 94.8},
-    {"State": "North Carolina", "Insured": 89.6},
-    {"State": "North Dakota", "Insured": 92.1},
-    {"State": "Ohio", "Insured": 93.5},
-    {"State": "Oklahoma", "Insured": 86.2},
-    {"State": "Oregon", "Insured": 93.9},
-    {"State": "Pennsylvania", "Insured": 94.5},
-    {"State": "Rhode Island", "Insured": 95.7},
-    {"State": "South Carolina", "Insured": 90},
-    {"State": "South Dakota", "Insured": 90.5},
-    {"State": "Tennessee", "Insured": 90},
-    {"State": "Texas", "Insured": 82},
-    {"State": "Utah", "Insured": 91},
-    {"State": "Vermont", "Insured": 96.3},
-    {"State": "Virginia", "Insured": 93.2},
-    {"State": "Washington", "Insured": 93.6},
-    {"State": "West Virginia", "Insured": 93.9},
-    {"State": "Wisconsin", "Insured": 94.6},
-    {"State": "Wyoming", "Insured": 87.8}
-  ];
-
+  var barchart_data = d3.json("../data/insurancd2.json");
   var margin = { top: 30, right: 30, bottom: 70, left: 60 };
   var width = 800 - margin.left - margin.right;
   var height = 500 - margin.top - margin.bottom;
---
+++ js/visualization.js
@@ -0,0 +1,52 @@
+(() => {
+    // Include the code from barchart2.js
+    // ...
+  
+    // Include the code from scatterplot.js
+    // ...
+  
+    // Function to handle highlighting in the bar chart from the scatter plot
+    function highlightBarchart(selectedStates) {
+      // Implement the logic to highlight corresponding bars in the bar chart based on selected states in the scatter plot
+      // Use the selectedStates array to identify which bars to highlight
+      // You may need to modify this based on your data structure and logic
+      d3.selectAll('.bar')
+        .classed('selected', d => selectedStates.includes(d.State))
+        .attr('fill', d => (selectedStates.includes(d.State) ? 'red' : 'steelblue'));
+    }
+  
+    // Function to handle highlighting in the scatter plot from the bar chart
+    function highlightScatterplot(selectedStates) {
+      // Implement the logic to highlight corresponding circles in the scatter plot based on selected states in the bar chart
+      // Use the selectedStates array to identify which circles to highlight
+      // You may need to modify this based on your data structure and logic
+      d3.selectAll('.scatter-circle')
+        .classed('selected', d => selectedStates.includes(d.State))
+        .attr('fill', d => (selectedStates.includes(d.State) ? 'red' : ''));
+    }
+  
+    // Function to handle brushing in the bar chart
+    function brushBarchart(selectedStates) {
+      // Implement the logic to brush the corresponding bars in the bar chart based on selected circles in the scatter plot
+      // Use the selectedStates array to identify which bars to brush
+      // You may need to modify this based on your data structure and logic
+      d3.selectAll('.bar')
+        .classed('selected', d => selectedStates.includes(d.State))
+        .attr('fill', d => (selectedStates.includes(d.State) ? 'red' : 'steelblue'));
+    }
+  
+    // Function to handle brushing in the scatter plot
+    function brushScatterplot(selectedStates) {
+      // Implement the logic to brush the corresponding circles in the scatter plot based on selected bars in the bar chart
+      // Use the selectedStates array to identify which circles to brush
+      // You may need to modify this based on your data structure and logic
+      d3.selectAll('.scatter-circle')
+        .classed('selected', d => selectedStates.includes(d.State))
+        .attr('fill', d => (selectedStates.includes(d.State) ? 'red' : ''));
+    }
+  
+    // You may need to call these functions based on the interactions in your visualizations
+    // For example, when a user clicks on a bar in the bar chart, you can call highlightScatterplot(selectedStates)
+    // and when a user clicks on a circle in the scatter plot, you can call highlightBarchart(selectedStates)
+  })();
+  
